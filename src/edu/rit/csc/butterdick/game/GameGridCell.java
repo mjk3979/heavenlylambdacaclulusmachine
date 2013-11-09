b27339e0f7125d2f7d5fb1a5a2477997d8442234
Matthew Koontz <mjk3979@rit.edu>
2013-11-09 12:50:15 -0500

    Added a static method to convert expressions to grids.
    Haven't tested it yet though.

--- src/edu/rit/csc/butterdick/game/GameGridCell.java
+++ src/edu/rit/csc/butterdick/game/GameGridCell.java
@@ -1,12 +1,14 @@
 package edu.rit.csc.butterdick.game;
 
-import edu.rit.csc.butterdick.lambda.LambdaColor;
+import edu.rit.csc.butterdick.lambda.*;
 import android.graphics.Bitmap;
 
 import android.content.Context;
 
 public abstract class GameGridCell
 {
+	private static int SUB_GRID_SIZE = 100; // TODO: Somehow make this dyanamic
+
 	protected LambdaColor color;
 
 	public GameGridCell(LambdaColor color)
@@ -15,4 +17,36 @@ public abstract class GameGridCell
 	}
 
 	public abstract Bitmap getBitmap(Context ctxt);
+
+	public static GameGrid convertExpressionToGameGridCells(Expression expr)
+	{
+		GameGrid subGrid = new GameGrid(SUB_GRID_SIZE, SUB_GRID_SIZE);
+		convertExpressionToGameGridCellsHelper(expr, subGrid, 0, 0);
+		return subGrid;
+	}
+
+	private static int convertExpressionToGameGridCellsHelper(Expression expr, GameGrid subGrid, int rowOffset, int colOffset)
+	{
+		if (expr instanceof Variable)
+		{
+			subGrid.set(rowOffset, colOffset, new VariableGridCell(((Variable)expr).getColor()));
+			return 1;
+		}
+		else if (expr instanceof Lambda)
+		{
+			Lambda l = (Lambda)expr;
+			int width = convertExpressionToGameGridCellsHelper(l.getBody(), subGrid, rowOffset+1, colOffset);
+			subGrid.set(rowOffset, colOffset, new LambdaGridCell(l.getColor(), width));
+		}
+		else if (expr instanceof Pair)
+		{
+			Pair p = (Pair)expr;
+			int width = convertExpressionToGameGridCellsHelper(p.getFirst(), subGrid, rowOffset, colOffset);
+			width += convertExpressionToGameGridCellsHelper(p.getSecond(), subGrid, rowOffset, colOffset+width);
+			return width;
+		}
+
+		// Assert false
+		return 5/0;
+	}
 }
