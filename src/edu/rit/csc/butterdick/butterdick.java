cc76aa5196e70ea4ad99382259c8a4f1f1587c4f
Matthew Koontz <mjk3979@rit.edu>
2013-11-09 01:55:52 -0500

    Added files from `android create project`

---
+++ src/edu/rit/csc/butterdick/butterdick.java
@@ -0,0 +1,15 @@
+package edu.rit.csc.butterdick;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class butterdick extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
