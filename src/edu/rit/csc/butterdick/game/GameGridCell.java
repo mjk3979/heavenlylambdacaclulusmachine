361070d8352d8e847990813b0b837b2110185011
Matthew Koontz <mjk3979@rit.edu>
2013-11-09 14:47:28 -0500

    Revert "some bitmappery"
    This reverts commit d398cc6fe47e8206d89d7266ae0baa0b80c6e98a.

--- src/edu/rit/csc/butterdick/game/GameGridCell.java
+++ src/edu/rit/csc/butterdick/game/GameGridCell.java
@@ -1,43 +1,53 @@
 package edu.rit.csc.butterdick.game;
 
-import edu.rit.csc.butterdick.lambda.LambdaColor;
+import edu.rit.csc.butterdick.lambda.*;
 import android.graphics.Bitmap;
 
 import android.content.Context;
 
 public abstract class GameGridCell
 {
-	private Bitmap bmp;
+	private static int SUB_GRID_SIZE = 100; // TODO: Somehow make this dyanamic
+
 	protected LambdaColor color;
 
 	public GameGridCell(LambdaColor color)
 	{
 		this.color = color;
 	}
-	
-	public void setBmp(Bitmap image){
-		int width = image.getWidth();
-		int height = image.getHeight();
-		for (int i = 0; i < width; i++){
-			for (int y = 0; y < 6; y++)
-			image.setPixel(i, y, 0xffffff);
-		}
-		for (int i = 0; i < width; i++){
-			for (int y = height-6; y < height; y++)
-			image.setPixel(i, y, 0xffffff);
-		}
-		for (int i = 0; i < 6; i++){
-			for (int x = 0; x < height; x++)
-			image.setPixel(i, x, 0xffffff);
-		}
-		for (int i = width-6; i < width; i++){
-			for (int x = 0; x < height; x++)
-			image.setPixel(i, x, 0xffffff);
-		}
-		bmp = image;
+
+	public abstract Bitmap getBitmap(Context ctxt);
+
+	public static GameGrid convertExpressionToGameGrid(Expression expr)
+	{
+		GameGrid subGrid = new GameGrid(SUB_GRID_SIZE, SUB_GRID_SIZE);
+		convertExpressionToGameGridHelper(expr, subGrid, 0, 0);
+		return subGrid;
 	}
-	
-	public Bitmap getBitmap(Context ctxt){
-		return bmp;
+
+	private static int convertExpressionToGameGridHelper(Expression expr, GameGrid subGrid, int rowOffset, int colOffset)
+	{
+		if (expr instanceof Variable)
+		{
+			subGrid.set(rowOffset, colOffset, new VariableGridCell(((Variable)expr).getColor()));
+			return 1;
+		}
+		else if (expr instanceof Lambda)
+		{
+			Lambda l = (Lambda)expr;
+			int width = convertExpressionToGameGridHelper(l.getBody(), subGrid, rowOffset+1, colOffset);
+			subGrid.set(rowOffset, colOffset, new LambdaGridCell(l.getColor(), width));
+			return width;
+		}
+		else if (expr instanceof Pair)
+		{
+			Pair p = (Pair)expr;
+			int width = convertExpressionToGameGridHelper(p.getFirst(), subGrid, rowOffset, colOffset);
+			width += convertExpressionToGameGridHelper(p.getSecond(), subGrid, rowOffset, colOffset+width);
+			return width;
+		}
+
+		// Assert false
+		return 5/0;
 	}
 }
