73115b42b06d2c2dc57f33099412012c806f66e0
Matthew Koontz <mjk3979@rit.edu>
2013-11-09 03:58:21 -0500

    Added another lambda test.

--- src/edu/rit/csc/butterdick/test/LambdaTest.java
+++ src/edu/rit/csc/butterdick/test/LambdaTest.java
@@ -11,8 +11,16 @@ public class LambdaTest
 		return result.equals(new Lambda(LambdaColor.RED, new Variable(LambdaColor.RED)));
 	}
 
+	public static boolean test2()
+	{
+		Expression test = new Pair(new Lambda(LambdaColor.BLUE, new Pair(new Lambda(LambdaColor.RED, new Variable(LambdaColor.RED)), new Variable(LambdaColor.BLUE))), new Lambda(LambdaColor.YELLOW, new Variable(LambdaColor.YELLOW)));
+		Expression result = test.eval(Lambda.getEmptyContext());
+		return result.equals(new Lambda(LambdaColor.YELLOW, new Variable(LambdaColor.YELLOW)));
+	}
+
 	public static void main(String[] args)
 	{
 		System.out.printf("Test 1: %s\n", test1() ? "PASS" : "FAIL");
+		System.out.printf("Test 2: %s\n", test2() ? "PASS" : "FAIL");
 	}
 }
